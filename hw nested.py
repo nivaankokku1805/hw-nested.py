27bf77bcf4aaf31580cb82cdda87b203e42f82f4
nivaankokku1805 <reetu18@gmail.com>
2025-07-25 22:33:36 +0530

    Add files via upload

---
+++ hw nested.py
@@ -0,0 +1,14 @@
+Age_of_the_user =input(" Do you know your age, is it between 10 to 50 yes or no:")
+
+
+atten = int(input("Enter your age:"))
+
+if Age_of_the_user  == 'yes' : 
+    print("You are a grown up ")
+    
+else:
+    if atten<=51:
+        print("you are old ")   
+    else:
+        print("You are old")    
+        
